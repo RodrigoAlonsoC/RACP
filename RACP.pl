478e758099dd926ce76d5e694a0f7430a67820d1
RodrigoAlonzo <alonzo_5sepro@hotmail.com>
2015-09-25 19:25:26 -0500

    Update RACP.pl

--- RACP.pl
+++ RACP.pl
@@ -1,9 +1,7 @@
 #Created by Rodrigo C. 
 #Ultima actualizacion : 
 # 25/09/2015
-# 12:15 p.m
-
-
+# 7:25 p.m
 
 use LWP::Simple;
 use feature qw(switch say);
@@ -22,6 +20,10 @@ print "
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    [!] Aviso Legal :
+    Mi persona no se hace responsable de cualquier mal uso o daño causado por este programa.
+    [!] Legal Disclaimer :
+    My person is not responsible for any misuse or damage caused by this program.
 Contact
 |========================================|
 ||||||||||||||-----RACP-----||||||||||||||
@@ -30,14 +32,31 @@ Contact
 Facebook : https://www.fb.com/RodrigoAlonzoWar .
 Autor : Rodrigo Canaza .
 \n";
+$file = 'mhe\dis.txt';
+if ( -z $file  ){ 
 
-print "[!]Preciona cualquier Tecla para continuar...  ";
-$tecla = <STDIN>;
+
+	print "[!]Aceptas los terminos ? \n";
+print "[!]Preciona Si[S] o No[N]...\n";
+$respuesta = <STDIN>;
+chomp($respuesta);
+open (TER , ">$file") || die "[!] Algo fallo ...! \n";
+print TER "$respuesta";
+close(TER);
+print "Porfavor vuelve a ejecutar RACP! \n";
+
+
+
+	}
+else {
+
+open (TER , "<$file") || die "[!]Algo Fallo ...!\n";
+while ($response = <TER>){
+	if ($response =~ /S/ ||
+		$response =~ /s/){
+system('cls'); 
+
 print "\n\n\n";
-
-
-
-
 $msg =<<ETQT; 
                ####################################################
                #                                                  #
@@ -54,7 +73,7 @@ $msg =<<ETQT;
                #   ->Rodrigo Canaza . "RACP"                      #
                #   ->https://www.fb.com/RodrigoAlonzoWar          #
                #                                                  #
-               #  Version : 0.4                                   #
+               #  Version : 0.5                                   #
                #                                                  #
                ####################################################
 ETQT
@@ -413,5 +432,23 @@ else {
 	}
 close(ARCHIVO);
 	}	
+}
+}
+else { 
+
+	print "[!]Aceptas los terminos ? \n";
+print "[!]Preciona Si[S] o No[N]...\n";
+$respuesta = <STDIN>;
+chomp($respuesta);
+open (TER , ">$file") || die "[!] Algo fallo ...! \n";
+print TER "$respuesta";
+close(TER);
+print "Porfavor vuelve a ejecutar RACP! \n";
+
+
+
+}
+} 
+
 }
 
